425231604c094e0f617252a2a76fb8e4565b51f2
Muhammad Ali <muhammadalimirza671@gmail.com>
2026-05-15 12:07:33 +0500

    Update project for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import tailwindcss from '@tailwindcss/vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [tanstackStart(), tailwindcss(), react(), tsconfigPaths()],
   build: {
-    outDir: 'dist'
-  }
+    outDir: 'dist',
+    
+  },
+  server: {
+    port: 5173,   // ✅ always use 5173
+    strictPort: true // ✅ fail if 5173 is busy instead of auto-switching
+  },
 })
